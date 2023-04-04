d4fa5bfe048fa380f593ab15e2c88b24c4b765b2
TumenSan <sandanov.tumen.sib@mail.ru>
2023-04-04 15:57:13 +0700

    add new channel rmq

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+//брокер rabbitmq
 const amqp = require("amqplib");
 
-console.log("hello world");
+var channelConsumer, connection;
+
+connectQueue(); // call connectQueue function
+
+async function connectQueue() {
+    try {
+        //connect to 'test-queue', create one if does not exist already
+        connection = await amqp.connect("amqp://user1:password1@rabbitmq:5672");
+        channelConsumer = await connection.createConfirmChannel();
+
+        await channelConsumer.assertQueue("engine-to-estimation", { durable: true });
+
+        console.log('"engine-to-estimation"');
+
+        channelConsumer.consume("engine-to-estimation", async (data) => {
+            try {
+                let dataJson = JSON.parse(data.content.toString());
+                console.log("Received message: " + data.content.toString());
+
+                let est = "chess-est";
+
+                // Create the response object with the request ID for correlation
+                const response = {
+                    answer: est,
+                    requestId: dataJson.requestId
+                };
+
+                // Publish the response to the exchange with the routing key of the request ID
+                //await channelConsumer.sendToQueue("engine-to-estimation", Buffer.from(JSON.stringify(response)));
+
+                // Print information about the sent response
+                console.log("Response sent: " + JSON.stringify(response));
+
+                // Подтверждение обработки сообщения
+                await channelConsumer.ack(data);
+            } catch (error) {
+                console.log("Error occurred: " + error.message);
+                // Reject the message and return it to the queue to be reprocessed
+                channelConsumer.nack(data);
+            }
+
+        }, { noAck: false });
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
