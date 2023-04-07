5c9a8af6a5bca2ab088d5f2dd11ac365532f61e3
TumenSan <sandanov.tumen.sib@mail.ru>
2023-04-07 20:49:14 +0700

    attempt add estimate (just material)

--- index.js
+++ index.js
@@ -50,3 +50,64 @@ async function connectQueue() {
         console.log(error)
     }
 }
+
+class BoardInGame {
+    constructor (fen) {
+        this.fen = fen;
+        const parts = fen.split(' ');
+        const rows = parts[0].split('/');
+        console.log(rows);
+
+        this.board = {};
+        rows.forEach((row, i) => {
+            let col = 0;
+            row.split('').forEach((char) => {
+                let charParse = parseInt(char);
+                if (!isNaN(charParse)) {
+                    for (let j = 0; j < charParse; j++){
+                        const pos = `${String.fromCharCode('a'.charCodeAt() + col)}${8-i}`;
+                        this.board[pos] = null;
+                        ++col;
+                    }
+                } else {
+                    const pos = `${String.fromCharCode('a'.charCodeAt() + col)}${8-i}`;
+                    this.board[pos] = char;
+                    col++;
+                }
+            });
+        });
+        for (let i = 0; i < 8; i++){
+
+        }
+        //console.log(this.board);
+    }
+    
+    MaterialEstimate(){
+        let score = 0;
+        const materialValues = {
+            'P': 1,
+            'N': 3,
+            'B': 3,
+            'R': 5,
+            'Q': 9
+        };
+
+        for (let cell in this.board) {
+            if (this.board[cell]){
+                const piece = this.board[cell].charAt(0);
+                if (piece in materialValues) {
+                    score += materialValues[piece];
+                } else if (piece.toUpperCase() in materialValues) {
+                    score -= materialValues[piece.toUpperCase()];
+                }
+            }
+        }
+
+        console.log('Score:', score);
+    }
+}
+
+//const Board = new BoardInGame('r1bq1b1r/p4kpp/2n5/4p2n/8/8/PPPP1PPP/RNB1K2R w KQ - 0 11');
+//Board.MaterialEstimate();
+
+//fen: 'r1bqkb1r/ppp2ppp/2n2n2/3pp1N1/2B1P3/8/PPPP1PPP/RNBQK2R w KQkq - 0 5'
